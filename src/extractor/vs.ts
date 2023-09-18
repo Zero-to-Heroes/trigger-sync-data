220c620be9945422bb6b83a598b2c4d11bed8f48
sebastientromp <sebastien.tromp@gmail.com>
2023-09-18 20:36:41 +0200

    add safeguard for fall of ulduar mini-set

--- src/extractor/vs.ts
+++ src/extractor/vs.ts
@@ -13,9 +13,9 @@ export const extractViciousSyndicateStats = async (
 	prefs: Preferences,
 ): Promise<void> => {
 	// return;
-	// if (+message.buildNumber > 175000) {
-	// 	return;
-	// }
+	if (+message.buildNumber > 185054) {
+		return;
+	}
 
 	if (!prefs.shareGamesWithVS) {
 		return;
